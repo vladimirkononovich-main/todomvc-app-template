2e1c7bdff0d93bbe38feabccec6055025c5f4b10
vladimirkononovich-main <vova.kononovich.main@gmail.com>
2021-11-01 21:58:44 +0300

    refactor: change structure of the project

--- src/todos/header/headerInput.js
+++ src/todos/header/headerInput.js
@@ -2,12 +2,17 @@ import React, { useContext } from "react";
 import TodosContext from "../context/context";
 
 function HeaderInput() {
-  const { addNewTodo } = useContext(TodosContext);
+  const { newTodo } = useContext(TodosContext);
 
   const handleChange = (event) => {
     return event.target.value;
   };
-
+  const addNewTodo = (event) => {
+    if (event.code === "Enter" && event.target.value.trim()) {
+      newTodo(event)
+      event.target.value = null;
+    }
+  };
   return (
     <header className="header">
       <h1>todos</h1>
